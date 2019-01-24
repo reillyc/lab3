fd060ca2651a29761dd12c3ec7528c0a89dc7e7e
reillyc <35342845+reillyc@users.noreply.github.com>
2019-01-24 11:20:10 -0800

    Add files via upload

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,8 +10,52 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Experience Chan! It's not a lot of questions." +
+			"Too many questions is the Chan disease." +
+			"The best way is just to observe the noise of the world." +
+			"The answer to your questions?" +
+			"Ask your own heart.");
 	});
+
+console.log("Project clicked")
+
+function projectClick(e){
+	console.log("Project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+}
+
+
+console.log('hehe');
+  $("#myElement").click(changeText);
+  
+  function changeText(event){
+    $(this).text("Changed text");
+  }
+
+  /*
+  Adding a CSS class
+  
+  Select the element, then call addClass()
+  
+*/
+$('#css_change').click(function(){
+  $(this).prev().addClass("text-center");
+});
+
+/*
+  Remove, and toggle CSS Classes
+  You can remove classes with removeClass()
+  Or, toggle them on and off with toggleClass():
+*/
+
+$("#css_remove").click(function(){
+  $(this).prev().removeClass("text-center");
+});
+
+$("#css_toggle").click(function(){
+  $(this).prev().toggleClass("text-center");
+});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
